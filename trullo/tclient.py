cf0e40b2750e039138a0ebe369d3cdb905ad6d48
Philipp Metzner <pmetzner@freenet.de>
2026-02-16 21:14:25 +0100

    Fix TClient.delete

--- trullo/tclient.py
+++ trullo/tclient.py
@@ -42,7 +42,10 @@ class TClient:
 
     def delete(self, path: str) -> Dict:
         return self.handle_res(
-            requests.delete(self.base_url + path, self.build_auth_params()))
+            requests.delete(
+                self.base_url + path, data=self.build_auth_params()
+            )
+        )
 
     def put(self, path: str, **data) -> Dict:
         return self.handle_res(
